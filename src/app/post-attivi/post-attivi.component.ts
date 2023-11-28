bb33bafb11604e41d92f074956084da084d521ce
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-28 16:59:07 +0100

    commit

--- src/app/post-attivi/post-attivi.component.ts
+++ src/app/post-attivi/post-attivi.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from '../service/post.service';
 
 @Component({
   selector: 'app-post-attivi',
   templateUrl: './post-attivi.component.html',
-  styleUrls: ['./post-attivi.component.scss']
+  styleUrls: ['./post-attivi.component.scss'],
 })
 export class PostAttiviComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private postSrv: PostService) {}
+  post: any[] = [];
   ngOnInit(): void {
+    this.post = this.postSrv.post;
   }
-
 }
--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Post } from '../post';
 
+fetch('../assets/db.json')
+  .then((resp) => {
+    console.log(resp);
+    if (!resp) {
+      throw new Error();
+    }
+    return resp.json();
+  })
+  .then(console.log);
+
 @Injectable({
   providedIn: 'root',
 })
@@ -9,9 +19,8 @@ export class PostService {
 
   constructor() {}
 
-  postAttivi(): void {
+  newArray(id: number, body: string, title: string, active: boolean) {
     this.post.push({ id, body, title, active });
+    console.log(this.post);
   }
-
-  postInattivi() {}
 }
